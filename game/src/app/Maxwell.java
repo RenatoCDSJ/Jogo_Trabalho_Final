5dbdf4550d77b3262a7f62f27a2dd1d526b9760e
felixgusta <guto.felix2003@gmail.com>
2023-06-07 23:10:28 -0300

    2 modificaçao
    craiçao do metodo de onde vem()
    retirada da class moedaMaiorQueZero()

--- game/src/app/Maxwell.java
+++ game/src/app/Maxwell.java
@@ -1,32 +1,50 @@
 package app;
 
+import java.util.Stack;
+
 public class Maxwell {
     private Jewel Jewel;
     public int AmountCoins = 3;
+    // amount of coins =  Quantidade de moedas
 
-    // amount of coins Quantidade de moedas
-
-    public void getAmountCoins(int AmountCoins){
+    public void getAmountCoins(int AmountCoins) {
         this.AmountCoins = AmountCoins;
     }
-    public int setAmountCoins(int AmountCoins){
+
+    public int setAmountCoins(int AmountCoins) {
         return AmountCoins;
     }
 
-    
-    public void CurrencyFlow(){
-        // Fluxo de moedas
-        if (AmountCoins > 0) {
-           
-            System.out.println("Quantidade atual de moedas: " + AmountCoins);
+    public Stack<String> LastCity;
+
+    public void City() {
+        LastCity = new Stack<>();
+    }
+
+    public void WhereDidItComeFrom() {
+        // metodo de onde vem: que retorna a ultima cidade de onde Max veio
+        if (LastCity.isEmpty()) {
+            System.out.println("Nenhuma cidade foi visitada ainda.");
+
         } else {
-            System.out.println("Fim de jogo"); // Caso não haja moedas restantes
+           System.out.println("Ultima cidade:" + LastCity.peek());
+
         }
     }
-    
-    public void ticket(){
-        // passagem para a proxima cidade
-        this.AmountCoins --;
+
+    public void CurrencyFlow() {
+        // Fluxo de moedas
+        if (AmountCoins > 0) {
+
+            System.out.println("Quantidade atual de moedas: " + AmountCoins);
+        } else {
+            System.out.println("Fim de jogo");
+            // Caso não haja moedas restantes
+        }
     }
-    
+
+    public void ticket() {
+        // passagem para a proxima cidade
+        this.AmountCoins--;
+    }
 }
