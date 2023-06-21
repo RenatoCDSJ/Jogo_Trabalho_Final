db1d54d6e066cef5cc906cc32fed5f6f75746902
felixgusta <guto.felix2003@gmail.com>
2023-06-21 19:38:03 -0300

    Criação da Class Maxwell
    Criação da Class Maxwell possuindo os parametros( "amount Coins, powerJewel, powerthreshold)
    ela possui os metodos:
    
     	powerlessthanPowerLimit: que faz com que, se o poder da joia passar do limite o jogo acaba.
     	CurrencyFlow: ficara encarregado de controlar a quantidade de moedas, de modo que não deixe a quantidade ficar menor que 0.
     	nextCity: que vai retirar uma moeda a cada fronteira que Maxwell passar.

--- game/src/app/Main.java
+++ game/src/app/Main.java
@@ -2,6 +2,12 @@ package app;
 
 public class Main {
     public static void main(String[] args) {
+        Maxwell player = new Maxwell(3,7,0);
+       
+        
+
         System.out.println("Hello World, find my voice");
+    
+
     }
 }
--- game/src/app/Maxwell.java
+++ game/src/app/Maxwell.java
@@ -1,20 +1,18 @@
 package app;
 
-
 public class Maxwell {
+    public int AmountCoins; // amount of coins = Quantidade de moedas
+    public int powerJewel;
+    public int powerthreshold;
 
-    public Jewel Jewel;
-    public int Coins;
-    public int AmountCoins ; // amount of coins =  Quantidade de moedas
-    public String currentCity;
-
-// this is Maxwell
-
-    public Maxwell(int AmountCoins){
+    // this is playerMaxwell
+    public Maxwell(int AmountCoins, int powerJewel, int powerthreshold) {
         this.AmountCoins = 3;
+        this.powerJewel = powerJewel;
+        this.powerthreshold = powerthreshold;
     }
 
-
+    // Coins
     public void getAmountCoins(int AmountCoins) {
         this.AmountCoins = AmountCoins;
     }
@@ -22,31 +20,46 @@ public class Maxwell {
     public int setAmountCoins(int AmountCoins) {
         return AmountCoins;
     }
-    
+
+    // jewel [joia]
+    public void getpower(int powerJewel) {
+        this.powerJewel = powerJewel;
+    }
+
+    public int setpowerJewl(int powerJewel) {
+        return powerJewel;
+    }
+
+    // powerthreshold [Limite de energia]
+    public void getpowerthreshold(int powerthreshold) {
+        this.powerthreshold = powerthreshold;
+    }
+
+    public int setpowerthreshold(int powerthreshold) {
+        return powerthreshold;
+    }
+    // Jewel
+    // Metodos
+    // esse metodo faz com que, se o poder da joia passar do limite o game acaba
+    public void powerlessthanPowerLimit(int powerJewel, int powerthreshold) {
+        if (powerJewel < 0) {
+            powerJewel = 0;
+        } else if (powerJewel > powerthreshold) {
+            System.out.println("O poder da joia do poder foi de mais para o pobre Maxwell 'Game over'");
+        }
+    }
     public void CurrencyFlow() {
         // Fluxo de moedas
         if (AmountCoins > 0) {
-
             System.out.println("Quantidade atual de moedas: " + AmountCoins);
-        }
-         else {
-            System.out.println("Game over");
+        } else {
+            System.out.println("Maxwell nao possui moedas Game over");
             // Caso não haja moedas restantes
-        } 
+        }
     }
-    public void ticket() {
-        // passagem para a proxima cidade
+
+    public void nextCity() {
         // Ticket to the next city
         this.AmountCoins--;
-        
-    }
-    public void addCity(String city) {
-        currentCity = city;
-    }
-
-    public String toString() {
-        return "Cidade atual: " + currentCity;
     }
 }
-
-
