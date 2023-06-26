f644caa89e5adf2daac1b95d6eb156647be5b832
volturifox <51099698+volturifox@users.noreply.github.com>
2023-06-26 19:35:36 -0300

    Class Game
    Starting to implement and connect all the game classes

--- game/src/app/Game.java
+++ game/src/app/Game.java
@@ -2,13 +2,21 @@ package app;
 
 public class Game {
     public void play(){
+        boolean vivo = true;
+        Dialogues dialogues = new Dialogues();
         Quests quests = new Quests();
-        System.out.println(quests.list.get(0));
+        //System.out.println(quests.list.get(0));
         //loop
         //show the history
         //show the coins, jewel power and jewel threshold
         //show TesteGraph
         //show merchant
         //if the current city have a quest on the city show the quest
+        dialogues.inicio();
+        while(!vivo){
+            dialogues.clearTerminal();
+            
+        }
+        
     }
 }
