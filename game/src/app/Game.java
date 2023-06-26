81372ec675385625d5184c31a0c398a47c1f01cf
RenatoCDSJ <rjuniorrobattini@gmail.com>
2023-06-26 18:58:47 -0300

    Class Game
    Order of how play will work in the first instance.

--- game/src/app/Game.java
+++ game/src/app/Game.java
@@ -4,5 +4,11 @@ public class Game {
     public void play(){
         Quests quests = new Quests();
         System.out.println(quests.list.get(0));
+        //loop
+        //show the history
+        //show the coins, jewel power and jewel threshold
+        //show TesteGraph
+        //show merchant
+        //if the current city have a quest on the city show the quest
     }
 }
