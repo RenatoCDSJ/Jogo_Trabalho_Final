de5075d665961e2baf953a7f4f831370ce97d1e9
RenatoCDSJ <rjuniorrobattini@gmail.com>
2023-06-27 01:23:58 -0300

    Merchant class
    way to interact with class maxwell improved

--- game/src/app/Merchant.java
+++ game/src/app/Merchant.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class Merchant {
     //talks of the merchant
     //the merchant will need have in the parameters the coins of Maxwel
-    public CJ talk(int coin){
+    public void talk(Maxwell maxwel){
 
         System.out.println("Deseja trocar suas moedas por limiar da joia?");//get the anwser of player
         int trade = trade();
         
         Tree tree = new Tree();
-        CJ cj = tree.TreeMap(new CDT(coin, 1, trade));
-        return cj;
+        CJ cj = tree.TreeMap(new CDT(maxwel.getAmountCoins(), 1, trade));
+        int mCoins = maxwel.getAmountCoins();
+        int mJewel = maxwel.getPowerthreshold();
+        maxwel.setAmountCoins(mCoins + cj.getCoin());
+        maxwel.setPowerthreshold(mJewel + cj.getJewelThreshold());
     }
     public int trade(){
         Scanner scn = new Scanner(System.in);
-        String anwser = scn.nextLine();
-        if(anwser.equals("s")){
-            scn.close();
+        System.out.println("Aceitar: s/n");
+        String string = scn.nextLine();
+        if(string.equals("s")){
             return 1;
-        }else if(anwser.equals("n")){
-            scn.close();
+        }else if(string.equals("n")){
             return 0;
-        }else{
-            trade();
         }
-        scn.close();
+        trade();
         return 0;
     }
 }
